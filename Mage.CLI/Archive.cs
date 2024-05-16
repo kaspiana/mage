664308f942c9b551e6cc7c802ccc56212e7c7b24
kaspiana <fmcardiac@gmail.com>
2024-05-17 03:40:32 +1000

    add: db connection funcs

--- Mage.CLI/Archive.cs
+++ Mage.CLI/Archive.cs
@@ -5,6 +5,9 @@ namespace Mage.Engine;
 
 public struct Archive {
 
+    public const string INFO_FILE_PATH = "info";
+    public const string DB_FILE_PATH = "db.sqlite";
+
     public string mageDir;
     public string fileDir;
 
@@ -15,7 +18,7 @@ public struct Archive {
     public static Archive Load(string mageDir, string fileDir){
 
         var infoMap = new Dictionary<string, string>();
-        foreach(var line in File.ReadAllLines(mageDir + "info")){
+        foreach(var line in File.ReadAllLines($"{mageDir}{INFO_FILE_PATH}")){
             var splitIndex = line.IndexOf('=');
             var infoKey = line[..splitIndex];
             var infoValue = line[(splitIndex+1)..];
@@ -33,6 +36,21 @@ public struct Archive {
 
         return archive;
 
+    }
+
+    public void ConnectDB(){
+        if(db is null){
+            db = new SqliteConnection($"DataSource={mageDir}{DB_FILE_PATH}");
+            db.Open();
+        }
+    }
+
+    public void DiscnnectDB(){
+        if(db is not null){
+            db.Close();
+            db.Dispose();
+            db = null;
+        }
     }
 
 }
