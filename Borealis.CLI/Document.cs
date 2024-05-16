0592abf89153a78e0e0058a273988058a126a6d5
kaspiana <fmcardiac@gmail.com>
2024-05-16 21:13:49 +1000

    add: format and thumbnail members in document

--- Borealis.CLI/Document.cs
+++ Borealis.CLI/Document.cs
@@ -3,6 +3,8 @@ namespace Borealis.Engine;
 public struct Document {
     public string hash;
     public DocumentID? id;
+    public string format;
     public DateTime ingestDate;
     public string? comment;
+    public byte[]? thumbnail;
 }
