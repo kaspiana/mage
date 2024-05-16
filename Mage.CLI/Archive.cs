e2d9c813931067262f2100b22b117d511dcff403
kaspiana <fmcardiac@gmail.com>
2024-05-17 03:36:26 +1000

    add: archive load func

--- Mage.CLI/Archive.cs
+++ Mage.CLI/Archive.cs
@@ -7,6 +7,32 @@ public struct Archive {
 
     public string mageDir;
     public string fileDir;
-    public SqliteConnection db;
+
+    public string? name;
+
+    public SqliteConnection? db;
+
+    public static Archive Load(string mageDir, string fileDir){
+
+        var infoMap = new Dictionary<string, string>();
+        foreach(var line in File.ReadAllLines(mageDir + "info")){
+            var splitIndex = line.IndexOf('=');
+            var infoKey = line[..splitIndex];
+            var infoValue = line[(splitIndex+1)..];
+            infoMap[infoKey] = infoValue;
+        }
+
+        string? name = infoMap.ContainsKey("name") ? infoMap["name"] : null;
+
+        var archive = new Archive(){
+            mageDir = mageDir,
+            fileDir = fileDir,
+            name = name,
+            db = null
+        };
+
+        return archive;
+
+    }
 
 }
