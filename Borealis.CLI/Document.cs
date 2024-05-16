09a98d808bf2f5b48ca60a7bafe83669fccb46c5
kaspiana <fmcardiac@gmail.com>
2024-05-16 21:11:02 +1000

    remove: document constructor

--- Borealis.CLI/Document.cs
+++ Borealis.CLI/Document.cs
@@ -5,16 +5,4 @@ public struct Document {
     public DocumentID? id;
     public DateTime ingestDate;
     public string? comment;
-
-    public Document(
-        string _hash, 
-        DocumentID? _id, 
-        DateTime _ingestDate, 
-        string? _comment
-    ){
-        hash = _hash;
-        id = _id;
-        ingestDate = _ingestDate;
-        comment = _comment;
-    }
 }
