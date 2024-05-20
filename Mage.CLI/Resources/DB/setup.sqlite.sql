68ea173dd56c37148d0b0f20e4781344d106586c
kaspiana <fmcardiac@gmail.com>
2024-05-20 16:35:23 +1000

    tidy: sql db setup script

--- Mage.CLI/Resources/DB/setup.sqlite.sql
+++ Mage.CLI/Resources/DB/setup.sqlite.sql
@@ -1,43 +1,47 @@
+
+---
+--- DOCUMENTS
+---
+
 create table document (
-    id integer not null primary key autoincrement,
-    hash text not null,
-    file_name text not null,
-    extension text not null,
-    ingested_at integer not null,
-    comment text
+    id              integer not null primary key autoincrement,
+    hash            text not null,
+    file_name       text not null,
+    extension       text not null,
+    ingested_at     integer not null,
+    comment         text
+);
+
+---
+--- TAXONYMS
+---
+
+create table taxonym (
+	id                  integer not null primary key autoincrement,
+	canon_parent_id     integer,
+	canon_alias         text not null,
+
+	foreign key (canon_parent_id) references taxonym(id),
+	foreign key (id, canon_alias) references taxonym_alias(taxonym_id, alias) deferrable initially deferred,
+	foreign key (id, canon_parent_id) references taxonym_parent(child_id, parent_id) deferrable initially deferred,
+	unique(canon_parent_id, canon_alias)
 );
 
 create table taxonym_alias (
-	taxonym_id integer not null,
-	alias text not null,
+	taxonym_id  integer not null,
+	alias       text not null,
 
-	foreign key (taxonym_id) references taxonym(id)
-		deferrable initially deferred,
+	foreign key (taxonym_id) references taxonym(id) deferrable initially deferred,
 	primary key (taxonym_id, alias)
 );
 
 create table taxonym_parent (
-	child_id integer not null,
-	parent_id integer not null,
+	child_id    integer not null,
+	parent_id   integer not null,
 
-	foreign key (child_id) references taxonym(id)
-        deferrable initially deferred,
-	foreign key (parent_id) references taxonym(id)
-        deferrable initially deferred,
+	foreign key (child_id) references taxonym(id) deferrable initially deferred,
+	foreign key (parent_id) references taxonym(id) deferrable initially deferred,
 	primary key (child_id, parent_id)
-);
-
-create table taxonym (
-	id integer not null primary key autoincrement,
-	canon_parent_id integer,
-	canon_alias text not null,
-
-	foreign key (canon_parent_id) references taxonym(id),
-	foreign key (id, canon_alias) references taxonym_alias(taxonym_id, alias)
-		deferrable initially deferred,
-	foreign key (id, canon_parent_id) references taxonym_parent(child_id, parent_id)
-		deferrable initially deferred,
-	unique(canon_parent_id, canon_alias)
 );
 
 begin transaction;
@@ -45,41 +49,22 @@ insert into taxonym_alias (taxonym_id, alias) values (1, "");
 insert into taxonym (canon_parent_id, canon_alias) values (null, ""); -- root taxonym
 commit;
 
-
-
-
+---
+--- TAGS
+---
 
 create table tag (
-	id integer not null primary key autoincrement,
-	taxonym_id integer not null,
+	id          integer not null primary key autoincrement,
+	taxonym_id  integer not null,
 
 	foreign key (taxonym_id) references taxonym(id),
 	unique (taxonym_id)
-);
-
-create table document_tag (
-	document_id integer not null,
-    tag_id integer not null,
-	
-	foreign key (document_id) references document(id),
-	foreign key (tag_id) references tag(id),
-	primary key (document_id, tag_id)
 );
 
 create table tag_parameter (
 	tag_id integer not null primary key,
 
 	foreign key (tag_id) references tag(id)
-);
-
-create table document_tag_parameter (
-	document_id integer not null,
-    tag_id integer not null,
-	value integer not null,
-
-	foreign key (document_id, tag_id) references document_tag(document_id, tag_id),
-	foreign key (tag_id) references tag_parameter(tag_id),
-	primary key (document_id, tag_id)
 );
 
 create table tag_implication (
@@ -91,24 +76,51 @@ create table tag_implication (
 	primary key (antecedent_id, consequent_id)
 );
 
+create table document_tag (
+	document_id     integer not null,
+    tag_id          integer not null,
+	
+	foreign key (document_id) references document(id),
+	foreign key (tag_id) references tag(id),
+	primary key (document_id, tag_id)
+);
+
+create table document_tag_parameter (
+	document_id     integer not null,
+    tag_id          integer not null,
+	value           integer not null,
+
+	foreign key (document_id, tag_id) references document_tag(document_id, tag_id),
+	foreign key (tag_id) references tag_parameter(tag_id),
+	primary key (document_id, tag_id)
+);
+
+---
+--- COLLECTIONS
+---
+
 create table collection (
-	id integer not null primary key autoincrement,
-	title text,
-	comment text
+	id          integer not null primary key autoincrement,
+	title       text,
+	comment     text
 );
 
 create table collection_document (
-	collection_id integer not null,
-	document_id integer not null,
+	collection_id   integer not null,
+	document_id     integer not null,
 
 	foreign key (collection_id) references collection(id),
 	foreign key (document_id) references document(id),
 	primary key (collection_id, document_id)
 );
 
+---
+--- SOURCES
+---
+
 create table document_source (
-	document_id integer not null,
-	url text not null, 
+	document_id     integer not null,
+	url             text not null, 
 	
 	foreign key (document_id) references document(id),
 	primary key (document_id, url)
