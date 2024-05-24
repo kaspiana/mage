d10844cb4c9163b5912633ca6e23a94062aec97f
kaspiana <fmcardiac@gmail.com>
2024-05-24 14:01:17 +1000

    add: MediaType enum and mediaType field to Document model

--- Mage.CLI/Engine/Model/Document.cs
+++ Mage.CLI/Engine/Model/Document.cs
@@ -1,11 +1,21 @@
 namespace Mage.Engine;
 
+public enum MediaType {
+    Binary,
+    Text,
+    Image,
+    Animation,
+    Audio,
+    Video
+}
+
 public struct Document {
     public string hash;
     public DocumentID? id;
     public string fileName;
     public string fileExt;
     public int fileSize;
+    public MediaType mediaType;
     public DateTime addedAt;
     public DateTime updatedAt;
     public string? comment;
