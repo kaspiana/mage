4b0b07901b58029d6de8adc3cb62848f1138eac3
kaspiana <fmcardiac@gmail.com>
2024-05-24 19:42:02 +1000

    refactor: move migration sql fragments to app resources

--- Mage.CLI/Engine/Migration.cs
+++ Mage.CLI/Engine/Migration.cs
@@ -10,22 +10,7 @@ public class Migration {
         (SemanticVersion.FromString("alpha_12.0.0"), archiveDir => {
             var db = new DBEngine(){ dbPath = $"{archiveDir}{Archive.DB_FILE_PATH}" };
             db.EnsureConnected();
-
-            db.RunScript(@"
-                create table ranking (
-                    name            text not null primary key
-                );
-
-                create table document_ranking (
-                    document_id     integer not null,
-                    ranking_name    text not null,
-                    score           integer not null default 0,
-
-                    foreign key (document_id) references document(id),
-                    foreign key (ranking_name) references ranking(name),
-                    primary key (document_id, ranking_name)
-                );
-            ");
+            db.RunResourceScript("alpha_12.0.0.sqlite.sql");
 
             return true;
         })
