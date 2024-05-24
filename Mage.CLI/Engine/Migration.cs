d0a6b4bb8dc7be3e4b87b4c2051707f4a7e62279
kaspiana <fmcardiac@gmail.com>
2024-05-24 18:19:10 +1000

    fix: update archive version to exact build version upon successful migration, rather than normalised build version

--- Mage.CLI/Engine/Migration.cs
+++ Mage.CLI/Engine/Migration.cs
@@ -62,6 +62,11 @@ public class Migration {
             Console.WriteLine($"Unable to migrate from {archiveVersion} to {buildVersion}. (No script.)");
         }
 
+        if(archiveVersion == buildVersion){
+            infoMap["version"] = Archive.VERSION.ToString();
+            Archive.WriteInfoFile(archiveDir, infoMap);
+        }        
+
         return archiveVersion == buildVersion;
     }
 
