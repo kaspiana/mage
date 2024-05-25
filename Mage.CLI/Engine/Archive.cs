8e9c259315ffefd68e69a108b3364c9717b54b71
kaspiana <fmcardiac@gmail.com>
2024-05-26 00:11:59 +1000

    version: minor increase from alpha_15.0.1 to alpha_15.1.0

--- Mage.CLI/Engine/Archive.cs
+++ Mage.CLI/Engine/Archive.cs
@@ -17,8 +17,8 @@ public class Archive {
     public static readonly SemanticVersion VERSION = new SemanticVersion(){
         releaseType = -1,
         major = 15,
-        minor = 0,
-        patch = 1
+        minor = 1,
+        patch = 0
     };
 
     public const string IN_DIR_PATH = "in/";
