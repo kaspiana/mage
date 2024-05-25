6522486caaad58f418e4e91a10399d1fd42d6212
kaspiana <fmcardiac@gmail.com>
2024-05-25 22:52:28 +1000

    version: major increment from alpha_13.0.1 to alpha_14.0.0

--- Mage.CLI/Engine/Migration.cs
+++ Mage.CLI/Engine/Migration.cs
@@ -21,6 +21,14 @@ public class Migration {
             db.RunResourceScript("alpha_13.0.0.sqlite.sql");
 
             return true;
+        }),
+
+        (SemanticVersion.FromString("alpha_14.0.0"), archiveDir => {
+            var db = new DBEngine(){ dbPath = $"{archiveDir}{Archive.DB_FILE_PATH}" };
+            db.EnsureConnected();
+            db.RunResourceScript("alpha_14.0.0.sqlite.sql");
+
+            return true;
         })
 
     ];
