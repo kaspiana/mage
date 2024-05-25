fd93e089040ed48d7f3edf67b4afe0c1dfaf46d9
kaspiana <fmcardiac@gmail.com>
2024-05-25 22:51:50 +1000

    fix: use triggers to populate document_rating table

--- Mage.CLI/Resources/DB/setup.sqlite.sql
+++ Mage.CLI/Resources/DB/setup.sqlite.sql
@@ -214,3 +214,29 @@ create table document_rating (
     foreign key (ranking_name) references ranking(name),
     primary key (document_id, ranking_name)
 );
+
+create trigger trigger_populuate_document_rating_on_new_ranking
+after insert on ranking
+for each row begin
+    insert into document_rating (
+        document_id, 
+        ranking_name
+    )
+    select
+        document.id document_id,
+        new.name ranking_name
+    from document;
+end;
+
+create trigger trigger_populate_document_rating_on_new_document
+after insert on document
+for each row begin
+    insert into document_rating (
+        document_id,
+        ranking_name
+    )
+    select
+        new.id document_id,
+        ranking.name ranking_name
+    from ranking;
+end;
