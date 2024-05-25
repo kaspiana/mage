bac3ecf1cdbcdbb48911d2e1fedb0f55acda649a
kaspiana <fmcardiac@gmail.com>
2024-05-25 11:23:02 +1000

    fix: sql fragments for selecting rankings now account for missing rankings

--- Mage.CLI/Engine/DB/DBCommands.cs
+++ Mage.CLI/Engine/DB/DBCommands.cs
@@ -17,8 +17,51 @@ public static class DBCommands {
         public const string VideoMetadataWhereID = "select * from video_metadata where document_id = @document_id";
 
         public const string Ranking = "select * from ranking";
-        public const string DocumentRankingWhereID = "select ranking_name, score from document_ranking where document_id = @document_id";
-        public const string DocumentRankingWherePK = "select score from document_ranking where document_id = @document_id and ranking_name = @ranking_name";
+        public const string DocumentRankingWhereID = @"
+            select 
+                document_ranking_full.name ranking_name, 
+                ifnull(score, 0) score
+            from
+                (ranking cross join 
+                    (select 
+                        column1 document_id 
+                    from (values (@document_id)))
+                ) document_ranking_full
+                left join
+                document_ranking
+                on document_ranking_full.name = document_ranking.ranking_name
+                and document_ranking_full.document_id = document_ranking.document_id
+        ";
+        public const string DocumentRankingWhereName = @"
+            select 
+                document_ranking_full.id, 
+                document_ranking_full.ranking_name, 
+                ifnull(score, 0) score
+            from
+                (document cross join 
+                    (select 
+                        column1 ranking_name 
+                     from (values(@ranking_name)))
+                ) document_ranking_full
+                left join
+                document_ranking
+                on document_ranking_full.ranking_name = document_ranking.ranking_name
+                and document_ranking_full.id = document_ranking.document_id
+        ";
+        public const string DocumentRankingWherePK = @"
+            select 
+                ifnull(score, 0) score
+            from
+                (select 
+                    column1 document_id, 
+                    column2 ranking_name 
+                 from (values(@document_id, @ranking_name))
+                ) document_ranking_full
+                left join
+                document_ranking
+                on document_ranking_full.ranking_name = document_ranking.ranking_name
+                and document_ranking_full.document_id = document_ranking.document_id
+        ";
 
         public const string Taxonym = "select * from taxonym";
         public static string TaxonymIDClause(string clause) => $"select id from taxonym {clause}";
@@ -81,7 +124,7 @@ public static class DBCommands {
         public const string DocumentWherePK = "delete from document where id = @id";
 
         public const string RankingWherePK = "delete from ranking where name = @name";
-        public const string DocumentRankingWhereName = "delete from document_ranking where ranking_name = @ranking_name";
+        public const string DocumentRankingWhereName = @"delete from document_ranking where ranking_name = @ranking_name";
 
         public const string Taxonym = "delete from taxonym";
         public const string TaxonymWherePK = "delete from taxonym where id = @id";
